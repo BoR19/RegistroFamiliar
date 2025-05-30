d28fc94b9556861a8e74a43a72dceed4f777871e
bor-Hache <borbjo637@gmail.com>
2025-05-30 14:12:42 -0400

    cambio en el manifest

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'familia-cache-v1';
-const urlsToCache = ['/', '/index.html', '/script.js', '/styles.css'];
+const urlsToCache = ['/', '/index.html', '/script.js', '/styles.css','/manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
